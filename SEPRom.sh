4c9301261f2d3ec11acb33a0e61e07a96247c248
Palak <palakaggarwal18@gmail.com>
2021-06-22 15:33:40 +0530

    Minor changes in the script for new users

--- SEPRom.sh
+++ SEPRom.sh
@@ -1,13 +1,13 @@
-#pip install virtualenv
+pip install virtualenv
 
-#virtualenv --python=python3.8 SEProm_venv
+virtualenv --python=python3.8 SEProm_venv
 
 source SEProm_venv/bin/activate
 
-#pip install -r requirements.txt
+pip install -r requirements.txt
 
 python readAndCreateDF.py
 
-python dataset_ML.py
+#python dataset_ML.py
 
 deactivate
